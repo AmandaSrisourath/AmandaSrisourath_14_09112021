c9e9e1c58d7e7077b7dfdb3ffc8da8874d80d9b3
Amandozz <amanda.srisou@gmail.com>
2021-11-10 15:10:56 +1000

    add route EmployeeList

--- src/app/App.js
+++ src/app/App.js
@@ -6,7 +6,7 @@ import {
   Route,
 } from "react-router-dom";
 import CreateEmployee from "../employee/components/CreateEmployee";
-import CurrentEmployees from "../employee/components/CurrentEmployees";
+import EmployeeList from "../employee/components/EmployeeList";
 
 function App() {
   return (
@@ -16,8 +16,8 @@ function App() {
                   <CreateEmployee />
               </Route>
 
-              <Route path="/CurrentEmployees">
-                  <CurrentEmployees />
+              <Route path="/EmployeeList">
+                  <EmployeeList />
               </Route>
           </Switch>
       </Router>
