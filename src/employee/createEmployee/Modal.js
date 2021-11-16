625ad1aec97c73e9e7360f43d25280de7658cd5c
Amandozz <amanda.srisou@gmail.com>
2021-11-16 16:43:03 +1000

    add component Modal

---
+++ src/employee/createEmployee/Modal.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Modal from "react-modal";
+
+const customStyles = {
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
+};
+
+function CreateEmployeeModal(props) {
+    const { isOpen, onRequestClose } = props;
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            style={customStyles}
+        >
+            {/*<button onClick={closeModal}>X</button>*/}
+            <h2 className="modal">Employee Created!</h2>
+        </Modal>
+    )
+}
+
+export default CreateEmployeeModal;
