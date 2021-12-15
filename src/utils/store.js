abaa7c6c82ee3c0822cbeccb321fed70b359b627
Amandozz <amanda.srisou@gmail.com>
2021-12-15 15:00:59 +1000

    add middleware"

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,5 +1,5 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from "redux-persist";
+import { persistStore, persistReducer, FLUSH, REGISTER, REHYDRATE, PAUSE, PERSIST, PURGE } from "redux-persist";
 import userReducer from '../pages/createEmployee/employeeSlice';
 import storage from "redux-persist/lib/storage";
 
@@ -16,6 +16,12 @@ const persistedReducer = persistReducer(persistConfig, reducers);
 
 export const store = configureStore( {
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
 })
 
 export const persistor = persistStore(store);
